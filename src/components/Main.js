4e23a945337d3eec4981803a5ce117715903df34
Ghaidaamoh <ghaidaa97mohammad@gmail.com>
2021-06-30 18:25:28 +0300

    solve lab04

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,44 @@
 import React from 'react'
 import HornedBeast from './HornedBeast'
 import Horend from './Horend.json'
- class Main extends React.Component {
-    
+import FormSelect from './FormSelect';
+class Main extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            hornsRender: Horend,
+        }
+    }
+
+    sort = (event) => {
+        let pic = event.target.value;
+
+        let filterArray = [];
+
+        filterArray = Horend.filter(element => {
+            if (element.horns == pic) { return true }
+        }
+        )
+        this.setState({
+            hornsRender: filterArray
+        })
+
+        if (filterArray.length == 0) {
+            this.setState({
+                hornsRender: Horend
+            })
+        }
+    }
+
     render() {
-       
         return (
             <div>
-               {Horend.map((item,index)=>{
-                   return(
-                   <HornedBeast  title={item.title} prag={item.description} url={item.image_url} key={index}  handleShow={this.props.handleShow} modalData={this.props.modalData}/>
-                   )
-               })}
+                <FormSelect Filter={this.sort} />
+                {this.state.hornsRender.map((item, index) => {
+                    return (
+                        <HornedBeast title={item.title} prag={item.description} url={item.image_url} key={index} handleShow={this.props.handleShow} modalData={this.props.modalData} />
+                    )
+                })}
 
             </div>
 
