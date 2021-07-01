a6a1f47802a5f1834c3f1b4320b0514f4fbc733a
Ghaidaamoh <ghaidaa97mohammad@gmail.com>
2021-07-01 13:05:46 +0300

    fixed the issue

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,6 +1,7 @@
 import React from 'react' ;
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button'
 class HornedBeast extends React.Component {
     constructor(props){
 super(props);
@@ -11,17 +12,20 @@ this.state={
     numoflike = () => {
         this.setState({
             numoflikes: this.state.numoflikes + 1
-        })
+        })   
+    }
+    handleShow=() =>{
+        this.props.handleShow(this.props.title)
     }
     render() {
         return (
-            <div className="hornedbeasts">
+            <div className="hornedbeasts" style={{display:"inline-block", margin:"25px"}}>
 
                 <Card style={{ width: '18rem' }}>
                   
                     <Card.Body>
                         <Card.Title>{this.props.title}</Card.Title>
-                        <Card.Img  variant="top"  onClick={this.numoflike}  src={this.props.url} alt={this.props.title} />
+                        <Card.Img  variant="top"  onClick={this.numoflike} src={this.props.url} alt={this.props.title} />
                         <Card.Text>
                             {this.props.prag}
                         </Card.Text>
@@ -29,9 +33,9 @@ this.state={
                         Vote if you like me ...
                         </Card.Text>
                         <Card.Text>
-                        ❤ : {this.state.numoflikes}
+                        ❤ {this.state.numoflikes}
                         </Card.Text> 
-
+                        <Button variant="danger" onClick={this.handleShow}>Details</Button>
                     </Card.Body>
                 </Card>
             </div>
